3f3f04a35a76e1f4914b417747d0311a17b6ccc9
mcsterka <mcsterka@gmail.com>
2021-05-26 23:30:34 +0300

    Added all files of the project

---
+++ routes/users.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+const axios = require('axios');
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/privet', async function(req, res, next) {
+  console.log('PRIVET EBLISHE');
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  const result = await axios.get('http://dummy.restapiexample.com/api/v1/employees'); 
+  console.log("result", result.data);
+  res.send('darowa eblishe');
+})
+
+module.exports = router;
