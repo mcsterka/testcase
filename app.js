05485695a3324a1703acafe7389424dd315d93ee
mcsterka <mcsterka@gmail.com>
2021-05-29 17:15:06 +0300

    Added cors (without it POST method doesn't working). Added route for add employee;

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +20,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// const corsOptions ={
+//   origin:'http://localhost:3000', 
+//   credentials:true,            //access-control-allow-credentials:true
+//   optionSuccessStatus:200
+// }
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/users.js
+++ routes/users.js
@@ -15,5 +15,32 @@ router.get('/privet', async function(req, res, next) {
   .then(result => res.send(result.data))
   .catch(error => console.log(error))
 })
+router.post('/employee', async function(req, res, next) {
+  console.log('PRIVET2');
+  console.log('req:', req.body);
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  let result;
+  result = await axios.post("http://dummy.restapiexample.com/api/v1/create", req.body)
+  console.log("RESULT:", result.data.data);
+  let getAll;
+  getAll = await axios.get("http://dummy.restapiexample.com/api/v1/employees");
+  console.log("GETALL:", getAll.data.data);
+  res.send(getAll.data);
+  // setTimeout(axios.post("http://dummy.restapiexample.com/api/v1/create", req.body)
+  // .then(reslut => {
+  //   setTimeout(axios.get('http://dummy.restapiexample.com/api/v1/employees')
+  //   .then(result => res.send(result.data))
+  //   .catch(function(error){
+  //     console.log(error);
+  //     return res.send(error);
+  //   }), 3000)
+  // })
+  // .catch(function(error){
+  //   console.log(error);
+  //   return res.send(error);
+  // }), 3000)
+})
 
 module.exports = router;
