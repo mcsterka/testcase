ca5a0645b18a165b72f41c1fa670e8a361061c66
mcsterka <mcsterka@yandex.ru>
2021-06-16 18:24:29 +0300

    Deleted get routes

--- routes/users.js
+++ routes/users.js
@@ -6,15 +6,6 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/privet', async function(req, res, next) {
-  console.log('PRIVET');
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  axios.get('http://dummy.restapiexample.com/api/v1/employees')
-  .then(result => res.send(result.data))
-  .catch(error => console.log(error))
-})
 router.post('/employee', async function(req, res, next) {
   console.log('PRIVET2');
   console.log('req:', req.body);
